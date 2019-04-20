55404b6c0196a0685bd1a9f6ec80ca4c477b647a
Victor <platers81@gmail.com>
2019-04-19 23:54:07 -0400

    added course evaluation info

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 var classname = "field-name-field-prerequisite1";
 var pre = document.getElementsByClassName(classname)[0];
-var div=document.createElement("div"); 
+var div=document.createElement("div");
 div.classList.add("field");
 div.classList.add("field-name-field-prerequisite1");
 div.classList.add("field-type-text-with-summary");
@@ -12,7 +12,7 @@ bold.classList.add("field-label");
 bold.innerHTML = "Prerequisite for:&nbsp;";
 div.append(bold);
 
-var items = document.createElement("div"); 
+var items = document.createElement("div");
 items.classList.add("field-items");
 
 
@@ -21,13 +21,12 @@ items.append(p);
 
 
 div.append(items);
-console.log(pre);
 var parent = document.getElementsByClassName("content clearfix")[0];
 if(pre){
-	parent.insertBefore(div, pre.nextSibling); 
+	parent.insertBefore(div, pre.nextSibling);
 } else{
 	pre = document.getElementsByClassName("field-type-text-with-summary")[0];
-	parent.insertBefore(div, pre.nextSibling); 
+	parent.insertBefore(div, pre.nextSibling);
 }
 
 var courses = [];
@@ -59,16 +58,87 @@ $.ajax({	//request from cobalt api
         courses.sort();
         for(var i = 0; i < courses.length; i++){
         	var element = courses[i];
-        	console.log(element);
-			var a = document.createElement("a"); 
-			a.href = "/course/" + element;
-			a.target="_blank"
-			a.innerText = element;
-			if(i > 0) p.innerHTML += ', ';
-			p.append(a);
+        	//console.log(element);
+					var a = document.createElement("a");
+					a.href = "/course/" + element;
+					a.target="_blank"
+					a.innerText = element;
+					if(i > 0) p.innerHTML += ', ';
+					p.append(a);
         }
         if(courses.length == 0){
         	p.innerText = 'None';
         }
     }
 });
+
+
+
+
+//COURSE EVALUATIONS
+//DIFFICULTY
+var classname1 = "field-name-field-section-link";
+pre1 = document.getElementsByClassName(classname1)[0];
+var div1=document.createElement("div");
+div1.classList.add("field");
+div1.classList.add("field-name-field-prerequisite1");
+div1.classList.add("field-type-text-with-summary");
+div1.classList.add("field-label-inline");
+div1.classList.add("clearfix");
+
+var bold1 = document.createElement("div"); //Header
+bold1.classList.add("field-label");
+bold1.innerHTML = "Difficulty:&nbsp;";
+div1.append(bold1);
+
+var items1 = document.createElement("div");
+items1.classList.add("field-items");
+
+
+var p1 = document.createElement("p"); //List of courses
+items1.append(p1);
+
+
+div1.append(items1);
+parent.insertBefore(div1, pre1);
+
+
+//RATING
+var classname2 = "field-name-field-section-link";
+pre2 = document.getElementsByClassName(classname2)[0];
+var div2=document.createElement("div");
+div2.classList.add("field");
+div2.classList.add("field-name-field-prerequisite1");
+div2.classList.add("field-type-text-with-summary");
+div2.classList.add("field-label-inline");
+div2.classList.add("clearfix");
+
+var bold2 = document.createElement("div"); //Header
+bold2.classList.add("field-label");
+bold2.innerHTML = "Rating:&nbsp;";
+div2.append(bold2);
+
+var items2 = document.createElement("div");
+items2.classList.add("field-items");
+
+
+var p2 = document.createElement("p"); //List of courses
+items2.append(p2);
+
+
+div2.append(items2);
+parent.insertBefore(div2, pre2);
+
+function courseevals(json){
+	console.log(json);
+
+	console.log(json[code][0]);
+	p1.innerText = json[code][0].toFixed(1);
+	p2.innerText = json[code][1].toFixed(1);
+}
+
+const url = chrome.runtime.getURL('evals.json');
+
+fetch(url)
+    .then((response) => response.json()) //assuming file contains json
+    .then((json) => courseevals(json));
